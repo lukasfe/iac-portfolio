6dcd4d7e4537855eab7be576167c0163c026c746
lukasfe <lukasferencik@gmail.com>
2024-01-08 16:43:23 +0100

    found wrong s3 name

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -5,6 +5,7 @@ module "eks" {
   cluster_name    = local.cluster_name
   cluster_version = "1.27"
 
+
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
   cluster_endpoint_public_access = true
