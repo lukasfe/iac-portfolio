e1f2ca156e64d0456f317c242f1c072117252e8f
lukasfe <lukasferencik@gmail.com>
2024-01-08 17:03:55 +0100

    removed spacing to try fmt

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -4,28 +4,23 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.25.0"
     }
-
     tls = {
       source  = "hashicorp/tls"
       version = "~> 4.0.4"
     }
-
     cloudinit = {
       source  = "hashicorp/cloudinit"
       version = "~> 2.3.2"
     }
-
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 2.23.0"
     }
   }
-
   backend "s3" {
     bucket = "iac-portfolio"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
-
   required_version = "~> 1.6.3"
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,7 +3,6 @@ variable "region" {
   type        = string
   default     = "us-east-1"
 }
-
 variable "clusterName" {
   description = "Name of the EKS cluster"
   type        = string
