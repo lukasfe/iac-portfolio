b4d75c457d977cf1e1ba2211c79a17409d3eb1e7
lukasfe <lukasferencik@gmail.com>
2024-01-08 12:31:49 +0100

    "updated infra"

---
+++ terraform/vpc.tf
@@ -0,0 +1,24 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.2"
+
+  name = "portfolio-vpc"
+
+  cidr = "172.20.0.0/16"
+  azs  = slice(data.aws_availability_zones.available.names, 0, 1)
+
+  private_subnets  = ["172.20.1.0/24"]
+  public_subnets  = ["172.20.4.0/24"]
+
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = 1
+  }
+}
