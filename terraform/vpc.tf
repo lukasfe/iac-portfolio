92c522adff2f2d406568a071684b1c34aacd8e45
lukasfe <lukasferencik@gmail.com>
2024-01-08 17:25:12 +0100

    last test without apply

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -5,7 +5,7 @@ module "vpc" {
   cidr = "172.20.0.0/16"
   azs  = slice(data.aws_availability_zones.available.names, 0, 1)
   private_subnets  = ["172.20.1.0/24"]
-  public_subnets   = ["172.20.4.0/24"]
+  public_subnets   = ["172.20.2.0/24"]
   enable_dns_hostnames = true
   public_subnet_tags = {
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
